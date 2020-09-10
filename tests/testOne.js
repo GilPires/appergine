3e3e9ae07fcb59556aaa88b1358612b8b4cd4269
Gil <gilsantospires@gmail.com>
2020-09-10 10:42:52 +0200

    finish part1

--- tests/testOne.js
+++ tests/testOne.js
@@ -2,12 +2,40 @@ module.exports = {
     '@tags' : ['part1'],
     'test part one'(browser) {
         
+        
         browser
           .url('https://www.draw2d.org/draw2d/')
-          .click('link text', 'Examples')
           .saveScreenshot('tests_output/one.png')
-          .click('.draw2d_exposed_link')
+
+          .click('link text', 'Examples')
           .saveScreenshot('tests_output/two.png')
+
+          .click('link text', 'Explore')
+          .saveScreenshot('tests_output/three.png')
+
+        browser.windowHandles(function (result) {
+            var handle = result.value[1];
+            browser.switchWindow(handle);
+        });
+        browser 
+          .assert.urlContains('http://www.draw2d.org/draw2d_touch/jsdoc_6/')
+          .saveScreenshot('tests_output/four.png')
+
+          .setValue('#search-field-inputEl','Undo & Redo')
+          .saveScreenshot('tests_output/four.png')
+          .click('#search-dropdown')
+          .saveScreenshot('tests_output/five.png')
+
+          .assert.urlContains('http://www.draw2d.org/draw2d_touch/jsdoc_6/#!/example/buildin_commandstack')
+          .saveScreenshot('tests_output/six.png')
+
+          
+
+    
+        
+          
+        
+        
 
 
     }
---
+++ tests/testTwo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '@tags' : ['part2'],
+    'test part two'(browser) {
+        
+        
+        browser
+          .url('http://www.draw2d.org/draw2d_touch/jsdoc_6/#!/example/buildin_commandstack')
+    }
+}
