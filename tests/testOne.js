2dcac4f627b7eeb231cf6461ab147c1e2ec656ba
Gil <gilsantospires@gmail.com>
2020-09-14 11:32:01 +0200

    final

--- tests/testOne.js
+++ tests/testOne.js
@@ -1,7 +1,7 @@
 module.exports = {
     '@tags' : ['part1'],
     'test part one'(browser) {
-        
+            
         
         browser
           .url('https://www.draw2d.org/draw2d/')
@@ -20,23 +20,16 @@ module.exports = {
         browser 
           .assert.urlContains('http://www.draw2d.org/draw2d_touch/jsdoc_6/')
           .saveScreenshot('tests_output/four.png')
-
+         
+          
           .setValue('#search-field-inputEl','Undo & Redo')
-          .saveScreenshot('tests_output/four.png')
           .click('#search-dropdown')
           .saveScreenshot('tests_output/five.png')
 
           .assert.urlContains('http://www.draw2d.org/draw2d_touch/jsdoc_6/#!/example/buildin_commandstack')
           .saveScreenshot('tests_output/six.png')
 
-          
-
     
-        
-          
-        
-        
-
 
     }
 }
--- tests/testTwo.js
+++ tests/testTwo.js
@@ -1,9 +1,55 @@
 module.exports = {
     '@tags' : ['part2'],
     'test part two'(browser) {
-        
-        
+        const startButton = '';
+        const endButton = '';
+        const canvas = '';
+        const squareStar = '';
+        const squreEnd = '';
+        const connectionStart = '';
+        const connectionEnd = '';
+
         browser
           .url('http://www.draw2d.org/draw2d_touch/jsdoc_6/#!/example/buildin_commandstack')
+          /*
+          //.useXpath() or .useCss()
+          .waitForElementVisible(startButton)
+          
+          .moveToElement(startButton, 0, 0)
+          .mouseButtonDown(0)
+          .pause(5000)
+          .moveTo(canvas, 0, 0)
+          .mouseButtonUp(0) 
+          .pause(5000)
+          .click(squareStar)
+          .getElement(squareStar).setAttribute('cx', '150') // Not sure about this command
+          .getElement(squareStar).setAttribute('cy', '150') // Not sure about this command
+          .assert.attributeEquals(squareStar, 'cx', '150')
+          .assert.attributeEquals(squareStar, 'cy', '150')
+          .saveScreenshot('tests_output/1.png')
+
+          .moveToElement(endButton, 0, 0)
+          .mouseButtonDown(0)
+          .pause(5000)
+          .moveTo(canvas, 250, 0)
+          .mouseButtonUp(0) 
+          .pause(5000)
+          .click(squareEnd)
+          .getElement(squareEnd).setAttribute('cx', '150') // Not sure about this command
+          .getElement(squareEnd).setAttribute('cy', '150') // Not sure about this command
+          .assert.attributeEquals(squareEnd, 'cx', '150')
+          .assert.attributeEquals(squreEnd, 'cy', '150')
+          .saveScreenshot('tests_output/2.png')
+
+          .moveToElement(connectionStart, 0, 0)
+          .mouseButtonDown(0)
+          .pause(5000)
+          .moveTo(connectionEnd, 0, 0)
+          .mouseButtonUp(0) 
+          .pause(5000)
+          // Missing assertion
+          .saveScreenshot('tests_output/3.png')
+          */
+        
     }
 }
